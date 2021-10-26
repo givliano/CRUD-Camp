cca2b526f99d2efb89b2936b6b3d6f9de4d9fa0d
Giuliano Machado <giufma@gmail.com>
2021-10-26 11:09:33 -0300

    fix method calling instead of passing reference

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 router.get('/', catchAsync(campgrounds.index));
@@ -18,6 +17,6 @@ router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditF
 
 router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground()));
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
